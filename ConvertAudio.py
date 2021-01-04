57502b28c4780ac033c8f948a238b0ed9908a334
Will <wsblack1@gmail.com>
2021-01-03 20:17:15 -0700

    Working version rev2.0
    This version will work at the top level music folder and convert all files within each folder and copy metadata. Could technically be the first version that will actually work for a music library

--- ConvertAudio.py
+++ ConvertAudio.py
@@ -6,33 +6,47 @@ from mutagen.easyid3 import EasyID3 as eid3
 # will use to recursively search music folder
 import os
 
-# just call audio converter
-print("Converting... ")
-os.system("audioconvert convert .\ .\ --output-format .mp3") # eventually change to all python
+# for root, dirs, files in os.walk 
+rootList = []
 
 for root, dirs, files in os.walk(".", topdown = False):
-    for name in files:
-        if name.endswith(".m4a"):
+	for file in files:
+		if root not in rootList:
+			rootList.append(root)
 
-            # read metadata of m4a file
-            print("Copying metadata for " + name)
-            tagM4A = tt.get(name)
+#print(rootList)
+lenList = len(rootList)
 
-            # copy metadata between m4a and mp3
-            tempName = name.removesuffix('.m4a')
-            mp3file = tempName + '.mp3'
-            tagMP3 = eid3(mp3file)
+for dir in rootList[0:lenList-1]:
+	#print(dir)
+	cmd = "audioconvert convert " + '"' + dir + '"' + " " + '"' + dir + '"' + " --output-format .mp3"
+	print(cmd)
+	os.system(cmd) #eventually need to change to all python
 
-            # print eid3.valid_keys.keys()
-            # key tags to copy
-            tagMP3['title']       = tagM4A.title
-            tagMP3['album']       = tagM4A.album
-            #tagMP3['discnumber']  = tagM4A.disc #doesn't work for some reason. might have to do with NULL exception
-            tagMP3['artist']      = tagM4A.artist
-            tagMP3['albumartist'] = tagM4A.albumartist
-            tagMP3['date']        = tagM4A.year
-            tagMP3['genre']       = tagM4A.genre
-            tagMP3['tracknumber'] = tagM4A.track            
-            tagMP3.save()
+for root, dirs, files in os.walk(".", topdown = False):
+	for file in files:
+		if file.endswith(".m4a"):
 
-            print("done.")
+			# read metadata of m4a file
+			newfile = os.path.join(root,file)
+			print("Copying metadata for " + newfile)
+			tagM4A = tt.get(os.path.join(root,file))
+		
+			# copy metadata between m4a and mp3
+			tempName = newfile.removesuffix('.m4a')
+			mp3file = tempName + '.mp3'
+			tagMP3 = eid3(mp3file)
+		
+			# print eid3.valid_keys.keys()
+			# key tags to copy
+			tagMP3['title']       = tagM4A.title
+			tagMP3['album']       = tagM4A.album
+			#tagMP3['discnumber']  = tagM4A.disc #doesn't work for some reason. might have to do with NULL exception
+			tagMP3['artist']      = tagM4A.artist
+			tagMP3['albumartist'] = tagM4A.albumartist
+			tagMP3['date']        = tagM4A.year
+			tagMP3['genre']       = tagM4A.genre
+			tagMP3['tracknumber'] = tagM4A.track            
+			tagMP3.save()
+		
+			print("done.")
